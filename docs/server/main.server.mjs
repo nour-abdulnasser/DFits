551088f2a13c7bf086db3f98b5693a29feee520f
n0urs <20nourabdulnasser00@gmail.com>
2024-10-28 11:15:29 +0300

    Updated docs for GitHub Pages deployment

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZDO4NQZM.mjs";import"./chunk-B2NFCV2J.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-W3WANART.mjs";import"./chunk-B2NFCV2J.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
